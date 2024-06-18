2e90e005f2bdbb245a689b99daa83a52eeb2b53f
belouso4 <kibelousov495@proton.me>
2024-06-18 19:11:51 +0700

    UpgradeableProxy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import '@openzeppelin/hardhat-upgrades';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
